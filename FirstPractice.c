bec6ac8e524787530dfcd119a7a06aaf90a7800f
AliYahyaAlizada <154955919+AliYahyaAlizada@users.noreply.github.com>
2024-02-22 12:04:24 +0430

    Update FirstPractice.c

--- FirstPractice.c
+++ FirstPractice.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     printf("Aslam Alikum ! Welcome to our First practice about our project");
-    printf("Our project name is File System Map");
     printf("I will try my best to solve this project as well as possible.");
     printf("We should try to do it for learn something");
+    printf("We can start the work of our project when we find some information.");
 }
